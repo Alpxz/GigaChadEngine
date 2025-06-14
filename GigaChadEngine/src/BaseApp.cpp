e8a10f815523fd292a9071f47339fba15b4bcfae
Alpxz <oli.gamedevelopment@gmail.com>
2025-06-13 19:54:24 -0600

    Creacion de Base App y Device
    Se implementaron las siguientes funciones en device:
    - CreateRenderTargetView
    - CreateTexture2D
    - CreateDepthStencilView

---
+++ GigaChadEngine/src/BaseApp.cpp
@@ -0,0 +1,26 @@
+#include "BaseApp.h"
+
+
+HRESULT BaseApp::init()
+{
+	return E_NOTIMPL;
+}
+
+void BaseApp::update()
+{
+}
+
+void BaseApp::render()
+{
+}
+
+void BaseApp::destroy()
+{
+}
+
+int BaseApp::run(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int mCmdShow, WNDPROC wndproc)
+{
+
+
+	return 0;
+}
